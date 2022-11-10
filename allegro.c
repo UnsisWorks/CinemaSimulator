a6310069f951e1d8bcb6e0e00a9b3d3e2e3884d3
unsis <jared0192galicia@gmail.com>
2022-11-10 17:50:11 -0600

    Start Allegro

---
+++ allegro.c
@@ -0,0 +1,24 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
+ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_EVENT_QUEUE *events = NULL;
+
+int main() {
+    al_init();
+    al_install_keyboard();
+    al_set_new_display_flags(ALLEGRO_RESIZABLE);
+    al_set_new_display_option(ALLEGRO_SINGLE_BUFFER, 1, ALLEGRO_REQUIRE);
+    display = al_create_display(640, 480);
+    al_init_primitives_addon();
+
+    events = al_create_event_queue();
+    al_register_event_source(events, al_get_keyboard_event_source());
+
+    while (al_is_event_queue_empty(events)) {
+        al_draw_filled_circle(rand() % 640, rand() % 480, rand() % 64, al_map_rgb(rand() % 256, 0, 0));
+        al_flip_display();
+        al_rest(0.015);
+    }
+}
+// Compiler: gcc allegro.c -I/home/monstruosoft/libs/usr/local/include/ -L/home/monstruosoft/libs/usr/local/lib/ -lallegro -lallegro_primitives -o allegro
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <gtk/gtk.h>
-#include <mysql.h>
+// #include <mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
 // #include "Cola.c"
@@ -24,9 +24,9 @@ static void initSimulator (GtkApplication *app, gpointer user_data) {
  */
 static void activate (GtkApplication *app, gpointer user_data) {
     
-    GtkWidget *buttBoxAcer, *buttBoxPlay, *buttBoxHelp, *buttBoxScore, *buttBoxExit, *box;
+    GtkWidget *buttBoxAcer, *buttBoxPlay, *buttBoxHelp, *buttBoxScore, *buttBoxExit, *box, *subLine;
     GtkWidget *buttonAcer, *buttonStart, *buttonHelp, *buttonList, *buttonExit;
-    GtkWidget *imageExit, *imageAcer, *imagePlay, *imageScore, *imageSett;
+    GtkWidget *imageExit, *imageAcer, *imagePlay, *imageScore, *imageSett, *labelTitle, *fixed;
     GtkCssProvider *cssProvider;
     // Load images for buttons
     imagePlay = gtk_image_new_from_file("./image/PLAY.png");
@@ -36,10 +36,17 @@ static void activate (GtkApplication *app, gpointer user_data) {
     imageSett = gtk_image_new_from_file("./image/SETTINGS.png");
 
     cssProvider = gtk_css_provider_new();
+    subLine = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
 
     // Create container fixed
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
+
+    labelTitle = gtk_label_new("Cinema simulator");
+    fixed = gtk_fixed_new();
+    gtk_fixed_put(GTK_FIXED(fixed), labelTitle, 0, 0);
+    gtk_fixed_put(GTK_FIXED(fixed), subLine, 0, 40);
+    gtk_box_pack_start(GTK_BOX(box), fixed, TRUE, FALSE, 30);
 
     // Create box for buttons
     buttBoxAcer = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
@@ -76,10 +83,10 @@ static void activate (GtkApplication *app, gpointer user_data) {
     
     // Add buttons at box
     gtk_container_add(GTK_CONTAINER(box), buttBoxPlay);
-    gtk_container_add(GTK_CONTAINER(box), buttBoxScore);
-    gtk_container_add(GTK_CONTAINER(box), buttBoxHelp);
-    gtk_container_add(GTK_CONTAINER(box), buttBoxAcer);
-    gtk_container_add(GTK_CONTAINER(box), buttBoxExit);
+    // gtk_container_add(GTK_CONTAINER(box), buttBoxScore);
+    // gtk_container_add(GTK_CONTAINER(box), buttBoxHelp);
+    // gtk_container_add(GTK_CONTAINER(box), buttBoxAcer);
+    // gtk_container_add(GTK_CONTAINER(box), buttBoxExit);
 
     gtk_widget_set_size_request(GTK_WIDGET(buttonAcer), 240, 90);
     gtk_widget_set_size_request(GTK_WIDGET(buttonHelp), 240, 90);
@@ -92,16 +99,18 @@ static void activate (GtkApplication *app, gpointer user_data) {
     gtk_style_context_add_class(gtk_widget_get_style_context(buttonList), "button");
     gtk_style_context_add_class(gtk_widget_get_style_context(buttonExit), "button");
     gtk_style_context_add_class(gtk_widget_get_style_context(imageExit), "images");
+    gtk_style_context_add_class(gtk_widget_get_style_context(labelTitle), "labelTitle");
         
+    gtk_widget_set_name(GTK_WIDGET(box), "subLine");
+    gtk_widget_set_name(GTK_WIDGET(box), "box");
     // Set properties for winow
     mainWindow = gtk_application_window_new (app);
     gtk_window_set_position(GTK_WINDOW(mainWindow), GTK_WIN_POS_CENTER);
-    gtk_window_set_title (GTK_WINDOW (mainWindow), "torreAs");
-    gtk_window_set_default_size (GTK_WINDOW (mainWindow), 650, 850);
+    gtk_window_set_title(GTK_WINDOW (mainWindow), ".");
+    gtk_window_set_default_size (GTK_WINDOW (mainWindow), 800, 450);
     gtk_window_set_resizable(GTK_WINDOW(mainWindow), FALSE);
 
 
-    gtk_widget_set_name(GTK_WIDGET(box), "box");
 
     gtk_css_provider_load_from_path(cssProvider, "./style.css", NULL);
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
@@ -124,4 +133,4 @@ int main (int argc, char **argv) {
     return status;
 }
 
-// Compiler      
+// Compiler gcc `pkg-config --cflags gtk+-3.0` -o main main.c `pkg-config --libs gtk+-3.0`     
