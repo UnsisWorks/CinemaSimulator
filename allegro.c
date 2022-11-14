08ed8309ffdd7e5b14257bfc5dcb7755ffa61d70
jared0192galicia <jared0192galicia@gmail.com>
2022-11-13 21:51:57 -0600

    Set audio with Allegro

--- allegro.c
+++ allegro.c
@@ -1,25 +1,56 @@
+#include <stdio.h>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
-ALLEGRO_DISPLAY *display = NULL;
-ALLEGRO_EVENT_QUEUE *events = NULL;
+const float FPS = 60;
 
-int main() {
-    al_init();
-    al_install_keyboard();
-    al_set_new_display_flags(ALLEGRO_RESIZABLE);
-    al_set_new_display_option(ALLEGRO_SINGLE_BUFFER, 1, ALLEGRO_REQUIRE);
-    display = al_create_display(640, 480);
-    al_set_window_title(display, "window");
-    al_init_primitives_addon();
+int main(int argc, char *argv[])
+{
+	ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_SAMPLE *acodec = NULL;
+    ALLEGRO_SAMPLE_ID *id = NULL;
 
-    events = al_create_event_queue();
-    al_register_event_source(events, al_get_keyboard_event_source());
+    acodec = al_load_sample("./sound/sound.wav");
+	
+    // Initialize allegro
+	if (!al_init()) {
+		fprintf(stderr, "Failed to initialize allegro.\n");
+		return 1;
+	}
 
-    while (al_is_event_queue_empty(events)) {
-        al_draw_filled_circle(rand() % 640, rand() % 480, rand() % 64, al_map_rgb(rand() % 256, 0, 0));
-        al_flip_display();
-        al_rest(0.015);
+    // Initialize allegro
+    if (!al_init_acodec_addon()) {
+        fprintf(stderr, "Failed to initialize acodec.\n");
+        return 1;
     }
+
+    // Initialize allegro
+    if (!al_install_audio()) {
+        fprintf(stderr, "Failed to initialize audio.\n");
+        return 1;
+    }
+    
+    
+    // Initialize allegro
+    if (acodec == NULL) {
+        fprintf(stderr, "Failed to play audio.\n");
+        return 1;
+    }
+    al_play_sample(acodec, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
+    
+	// Create the display
+	// display = al_create_display(640, 480);
+	// if (!display) {
+	// 	fprintf(stderr, "Failed to create display.\n");
+	// 	return 1;
+	// }
+
+	
+
+	// Clean up
+	al_destroy_display(display);
+
+	return 0;
 }
-// Compiler: gcc allegro.c -I/home/monstruosoft/libs/usr/local/include/ -L/home/monstruosoft/libs/usr/local/lib/ -lallegro -lallegro_primitives -o allegro
+// Compiler: gcc allegro.c -I/home/monstruosoft/libs/usr/local/include/ -L/home/monstruosoft/libs/usr/local/lib/ -lallegro -lallegro_primitives -lallegro_audio -lallegro_acodec -o allegro
